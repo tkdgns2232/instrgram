817e6d7078086811d4fdbbf4e9e9b9a65117375b
tkdgns2232 <tkdgns2232@naver.com>
2025-01-02 17:46:17 +0900

    board

---
+++ html_css_js/board/js/list_service.js
@@ -0,0 +1,20 @@
+function loadList () {
+    let boardDatas = !!localStorage.getItem("boardDatas")
+    ? JSON.parse(localStorage.getItem("boardDatas"))
+    : [];
+
+    const boardList = document.querySelector(".board-list-container");
+    boardList.innerHTML = boardDatas.map(data => `
+         <li class="board-box">
+            <header class="board-header">
+                <h1>${data.title}(${data.writer})</h1>
+                
+            </header>    
+            <main class="borad-main">
+                <pre>${data.content}</pre>
+            </main>
+         </li>
+        `).join("");
+}
+
+loadList();
